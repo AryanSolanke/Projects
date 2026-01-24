eb51ca0b713d4ea648af90e7f644d9f7eae19a30
AryanSolanke <aryansolanke1812@gmail.com>
2026-01-24 23:23:11 +0530

    Enhance scientific calculator functionality: improve menu structure, add submenu for operations, and refine input validation

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,5 +1,6 @@
 from std import *
 from sci import *
+import textwrap
 
 def mode_choice_menu():
     print("\n======>Main Menu<======\n\n1. Standard\n2. Scientific\n3. Quit Calculator")
@@ -8,13 +9,30 @@ def std_calc_menuMsg():
     print("\n|=====>Operations<=====|\n\n1. Type expression\n2. Quit standard calculator\n\n|======================|")
 
 def sci_calc_menuMsg():
-    print("""\n|=========================>Operations<=========================|\n
-          1. Basic trigo functions(sinx,cosx,tanx...)
-          2. Inverse trigo functions(sin⁻¹x,cos⁻¹x,tan⁻¹x...)
-          3. Hyperbolic trigo functions(sinhx, coshx, tanhx...)
-          4. Inverse Hyperbolic trigo functions(sinh⁻¹x,cosh⁻¹x,tanh⁻¹x...)
-          5. Quit scientific calculator.
-          \n|===================================================================|""")
+    print(textwrap.dedent("""
+            |=========================>Operations<=========================|\n
+            1. Basic trigo functions(sinx,cosx,tanx...)
+            2. Inverse trigo functions(sin⁻¹x,cos⁻¹x,tan⁻¹x...)
+            3. Hyperbolic trigo functions(sinhx, coshx, tanhx...)
+            4. Inverse Hyperbolic trigo functions(sinh⁻¹x,cosh⁻¹x,tanh⁻¹x...)
+            5. Quit scientific calculator.\n
+            |==============================================================|\n"""))
+    
+def sci_calc_subMenu_msg(op_num):
+    match op_num:
+        case 1:
+            print("\n|=====>Operations<=====|\n\n1. sinx\n2. cosx\n3. tanx\n4. cotx\n5. secx\n6. cosecx\n\n|======================|")
+        case 2:
+            print("\n|=====>Operations<=====|\n\n1. sin⁻¹x\n2. cos⁻¹x\n3. tan⁻¹x\n4. cot⁻¹x\n5. sec⁻¹x\n6. cosec⁻¹x\n\n|======================|")
+        case 3:
+            print("\n|=====>Operations<=====|\n\n1. sinhx\n2. coshx\n3. tanhx\n4. cothx\n5. sechx\n6. cosechx\n\n|======================|")
+        case 4:
+            print("\n|=====>Operations<=====|\n\n1. sinh⁻¹x\n2. cosh⁻¹x\n3. tanh⁻¹x\n4. coth⁻¹x\n5. sech⁻¹x\n6. cosech⁻¹x\n\n|======================|")
+        case 5:
+            print("\nScientific calculator Sub-menu exited...")
+        case _:
+            errmsg()
+
 
 def std_calc_main_menu():
     while True:
@@ -39,7 +57,7 @@ def sci_calc_main_menu():
     while True:
         sci_calc_menuMsg()
         try:
-            op_num = int(input("Enter operation number: "))
+            op_num = int(input("Enter your choice: "))
             if not op_num:
                 errmsg()
         except (ValueError, KeyboardInterrupt, UnboundLocalError):
@@ -47,24 +65,22 @@ def sci_calc_main_menu():
             continue
         match op_num:
             case 1:
-                angle = get_angle()
-                print(f"\nsin({angle}) = {sine(angle)}")
+                sci_calc_subMenu_msg(op_num)
+                choice = int(input("Enter your choice: "))
+                eval_trigo_func(op_num, choice)
             case 2:
-                angle = get_angle()
-                print(f"\ncos({angle}) = {cosine(angle)}")
+                sci_calc_subMenu_msg(op_num)
+                choice = int(input("Enter your choice: "))
+                eval_trigo_func(op_num, choice)
             case 3:
-                angle = get_angle()
-                print(f"\ntan({angle}) = {tangent(angle)}")
+                sci_calc_subMenu_msg(op_num)
+                choice = int(input("Enter your choice: "))
+                eval_trigo_func(op_num, choice)
             case 4: 
-                angle = get_angle()
-                print(f"\ncot({angle}) = {cot(angle)}")
+                sci_calc_subMenu_msg(op_num)
+                choice = int(input("Enter your choice: "))
+                eval_trigo_func(op_num, choice)
             case 5:
-                angle = get_angle()
-                print(f"\nsec({angle}) = {sec(angle)}")
-            case 6:
-                angle = get_angle()
-                print(f"\ncosec({angle}) = {cosec(angle)}")
-            case 7:
                 print("\nScientific calculator closed!")
                 break
             case _:
--- Calculator/sci.py
+++ Calculator/sci.py
@@ -5,24 +5,115 @@ def get_angle():
     try:
         angle = float(input("Enter angle(degrees): "))
         return angle
-    except (ValueError, SyntaxError):
+    except (ValueError, SyntaxError, TypeError):
         errmsg()
 
 #Takes values for inverse trigo functions
-def get_val(choice):
+def get_val():
     try:
-        val = float(input("Enter angle(degrees): "))
-        if val<-1 or val>1:
-            errmsg()
-            print("Hyperbolic functions only take values between [-1,1]")
-            return None
-        else:
-            return val
-    except (ValueError, SyntaxError):
+        val = float(input("Enter x: "))
+        return val
+    except (ValueError, SyntaxError, TypeError):
         errmsg()
 
-def validate_val():
-    pass
+def validate_val(op_num, choice, val):
+    match op_num:
+        case 1:
+            return 1
+        case 2:
+            if (choice==1 or choice==2):
+                if val<-1 or val>1:
+                    print("Domain error: Enter value between [-1,1]")
+                    return 0
+                else:
+                    return 1
+            elif choice==3:
+                return 1
+            elif choice==4 or choice==5:
+                if val>-1 or val<1:
+                    print("Domain error: Enter value which don't lie between [-1,1]")
+                    return 0
+                else:
+                    return 1
+            elif choice==6:
+                return 1
+            else:
+                errmsg()
+        case 3:
+            return 1
+        case 4:
+            if choice==1:
+                return 1
+            if choice==2:
+                if val<1:
+                    print("Domain error: Enter value greater than 1")
+                    return 0
+                else:
+                    return 1
+            elif choice==3:
+                if val<-1 or val>1:
+                    print("Domain error: Enter value between [-1,1]")
+                    return 0
+                else:
+                    return 1
+            elif choice==4:
+                if val<=0 or val>1:
+                    print("Domain error: Enter value between (0,1]")
+                    return 0
+                else:
+                    return 1
+            elif choice==5:
+                if val==0:
+                    print("Domain error: Enter any value except 0")
+                    return 0
+                else:
+                    return 1
+            elif choice==6:
+                if val<1 or val>-1:
+                    print("Domain error: Enter value which don't lie between [-1,1]")
+                    return 0
+                else:
+                    return 1
+            else:
+                errmsg()
+        case _:
+            errmsg()
+
+def eval_trigo_func(op_num, choice):
+    if op_num==1:
+        if choice in norm_trigo_funcs:
+            name, func = norm_trigo_funcs[choice]
+            angle = get_angle()
+            print(f"{name}({angle}) = {func(angle)}")
+        else:
+            errmsg()
+    elif op_num==2:
+        if choice in hyperbolic_trigo_funcs:
+            name, func = hyperbolic_trigo_funcs[choice]
+            val = get_val()
+            if validate_val(op_num, choice, val):
+                print(f"{name}({val}) = {func(val)}")
+        else:
+            errmsg()
+    elif op_num==3:
+        if choice in inv_trigo_funcs:
+            name, func = inv_trigo_funcs[choice]
+            val = get_val()
+            if validate_val(op_num, choice, val):
+                print(f"{name}({val}) = {func(val)}")
+        else:
+            errmsg()
+    elif op_num==4:
+        if choice in inv_hyperbolic_trigo_funcs:
+            name, func = inv_hyperbolic_trigo_funcs[choice]
+            val = get_val()
+            if validate_val(op_num, choice, val):
+                print(f"{name}({val}) = {func(val)}")
+        else:
+            errmsg()
+    else:
+        errmsg()
+    
 
 ####Normal trigo functions
 def sine(angle):
@@ -80,39 +171,38 @@ def sech_inv(val):
 def cosech_inv(val):
     return 1/asinh(val)
 
-trigo_funcs = {
+norm_trigo_funcs = {
     1: ("sin", sine),
     2: ("cos", cosine),
     3: ("tan", tangent), 
     4: ("cot", cot),
     5: ("sec", sec),
-    6: ("cosec", cosec),
+    6: ("cosec", cosec)
+    }
 
-    7: ("sinh", sineh),
-    8: ("cosh", cosineh),
-    9: ("tanh", tangenth),
-    10: ("coth", coth),
-    11: ("sech", sech),
-    12: ("cosech", cosech),
+hyperbolic_trigo_funcs = {
+    1: ("sinh", sineh),
+    2: ("cosh", cosineh),
+    3: ("tanh", tangenth),
+    4: ("coth", coth),
+    5: ("sech", sech),
+    6: ("cosech", cosech) 
+    }
 
-    13: ("sin⁻¹", sine_inv),
-    14: ("cos⁻¹", cosine_inv),
-    15: ("tan⁻¹", tangent_inv),
-    16: ("cot⁻¹", cot_inv),
-    17: ("sec⁻¹", sec_inv),
-    18: ("cosec⁻¹", cosec_inv),
-    
-    19: ("sinh⁻¹", sineh_inv),
-    20: ("cosh⁻¹", cosineh_inv),
-    21: ("tanh⁻¹", tangenth_inv),
-    22: ("coth⁻¹", coth_inv),
-    23: ("sech⁻¹", sech_inv),
-    24: ("cosech⁻¹", cosech_inv)
+inv_trigo_funcs = {
+    1: ("sin⁻¹", sine_inv),
+    2: ("cos⁻¹", cosine_inv),
+    3: ("tan⁻¹", tangent_inv),
+    4: ("cot⁻¹", cot_inv),
+    5: ("sec⁻¹", sec_inv),
+    6: ("cosec⁻¹", cosec_inv)
 }
 
-
-# choice = int(input("Enter choice: "))
-# if choice in sci_ops:
-#     name, func = sci_ops[choice]
-#     val = get_angle() # or get_number()
-#     print(f"{name}({val}) = {func(val)}")
+inv_hyperbolic_trigo_funcs = {
+    1: ("sinh⁻¹", sineh_inv),
+    2: ("cosh⁻¹", cosineh_inv),
+    3: ("tanh⁻¹", tangenth_inv),
+    4: ("coth⁻¹", coth_inv),
+    5: ("sech⁻¹", sech_inv),
+    6: ("cosech⁻¹", cosech_inv)
+}
